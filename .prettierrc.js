044dbadb1d1fbb7d7be92c372d70f99b40fae673
qpjoy996 <qpjoy996@163.com>
2024-12-23 15:30:28 +0800

    add prettier and format

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,20 @@
 // Documentation for this file: https://prettier.io/en/configuration.html
+const prettierPluginPath = require("./common/autoinstallers/rush-prettier/node_modules/prettier-plugin-packagejson");
 module.exports = {
   // We use a larger print width because Prettier's word-wrapping seems to be tuned
   // for plain JavaScript without type annotations
+  // semi: true,
   printWidth: 110,
 
   // Use .gitattributes to manage newlines
   endOfLine: "auto",
 
   // Use single quotes instead of double quotes
-  singleQuote: true,
+  singleQuote: false,
 
   // For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
   // to use them for arrays only
   trailingComma: "none",
+
+  plugins: [prettierPluginPath]
 };
